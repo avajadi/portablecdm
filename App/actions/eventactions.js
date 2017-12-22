88235cc43dbd923ce495fc41fd613aede07e4318
Pontus <pontus.stjernstrom@ri.se>
2017-12-22 11:32:11 +0100

    Started on eventactions.

---
+++ App/actions/eventactions.js
@@ -0,0 +1,5 @@
+import * as types from './types';
+import { checkResponse } from '../util/httpResultUtils';
+import { createTokenHeaders, createLegacyHeaders, getCert } from '../util/portcdmUtils';
+import { noSummary, hasEvents } from '../config/instances';
+import pinch from 'react-native-pinch';
