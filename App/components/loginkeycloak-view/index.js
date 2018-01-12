76c02a97f217439ce2fc0554ab66cd17487bb19d
Pontus <pontus.stjernstrom@ri.se>
2018-01-12 11:05:10 +0100

    Finally got SDK 24 to build on iOS and got pinch to work as well.

--- App/components/loginkeycloak-view/index.js
+++ App/components/loginkeycloak-view/index.js
@@ -106,7 +106,7 @@ class LoginKeyCloakView extends Component {
         });
 
         if(result.type == 'success') {
-            const authenticated = await loginKeycloak(result.code);
+            const authenticated = await this.props.loginKeycloak(result.code);
             if(authenticated) {
                 this.loginConfirmed();
             }
--- App/config/constants.js
+++ App/config/constants.js
@@ -4,7 +4,7 @@ const baseURI = 'https://maritimeid.maritimecloud.net/auth';
 
 /**STAGING */
 const staging = {
-    redirectURI: 'http://localhost:1337/authing.html',
+    redirectURI: 'https://auth.expo.io/@rise_viktoria/portcdm-app',
     clientID: '0.1-urn:mrn:stm:service:instance:viktoria:portablecdm',
     baseURI: 'https://staging-maritimeid.maritimecloud.net/auth',
 }
@@ -13,7 +13,7 @@ export default function constants(isStaging) {
     return {
         RedirectURI: isStaging ? staging.redirectURI : redirectURI,
         ClientID: isStaging ? staging.clientID : clientID,
-        MaritimeAuthURI: `${isStaging ? staging.baseURI : baseURI}/realms/MaritimeCloud/protocol/openid-connect/auth?client_id=${isStaging ? staging.clientID : clientID}&redirect_uri=${isStaging ? staging.redirectURI : redirectURI}&response_mode=fragment&response_type=code&scope=openid`,
+        MaritimeAuthURI: `${isStaging ? staging.baseURI : baseURI}/realms/MaritimeCloud/protocol/openid-connect/auth?client_id=${encodeURIComponent(isStaging ? staging.clientID : clientID)}&redirect_uri=${encodeURIComponent(isStaging ? staging.redirectURI : redirectURI)}&response_mode=fragment&response_type=code&scope=openid`,
         MaritimeTokenURI: `${isStaging ? staging.baseURI : baseURI}/realms/MaritimeCloud/protocol/openid-connect/token`,
         MaritimeLogoutURI: `${isStaging ? staging.baseURI : baseURI}/realms/MaritimeCloud/protocol/openid-connect/logout?redirect_uri=${encodeURIComponent(isStaging ? staging.redirectURI : redirectURI)}`,
     }
